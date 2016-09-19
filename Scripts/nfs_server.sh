376750a6020e79ea4b7c5dce952e77787c74ac4e
Adrian G Prado <adrian.garciaprado@nrg.com>
2016-09-19 15:37:08 -0500

    NFS server variables

--- Scripts/nfs_server.sh
+++ Scripts/nfs_server.sh
@@ -2,17 +2,19 @@
 
 # Define variables
 INVENTORY_FILE='Inventory/vagrant_servers'
-ANSIBLE_HOSTS='support-server'
+ANSIBLE_HOSTS_GROUP='nfs'
 
 PLAY_BOOK='Playbooks/extra_server.yml'
 TAGS='nfs'
 
 # ping test
-connect=$(ansible ${ANSIBLE_HOSTS} -m ping | awk '{print $3}')
+connect=$(ansible ${ANSIBLE_HOSTS_GROUP} -m ping | awk '{print $3}')
 
 if [[ "${connect}" = 'SUCCESS' ]]; then
-  ansible command
+  # ansible command
   ansible-playbook --inventory=$INVENTORY_FILE \
-  --tags=$TAGS                \
-  $PLAY_BOOK
+                   --tags=$TAGS                \
+                   $PLAY_BOOK
+else
+  echo 'Host ${ANSIBLE_HOSTS_GROUP} not rechable'
 fi
